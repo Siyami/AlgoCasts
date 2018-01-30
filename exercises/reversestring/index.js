94b9496bebe6e050645a5f178349dfb88da0c212
Siyami Avci <siyami.avci@ad.infosys.com>
2018-01-30 16:28:43 -0500

    add solution for reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str){
+//   const arr = [];
+//   let newString= "";
+//   for (let i = str.length - 1; i >=0; i--) {
+//     arr.push(str[i]);
+//   }
+//   for(let j=0; j<arr.length; j++){
+//     newString += arr[j];
+//   }
+//   return newString;
+// }
+
+function reverse(str){
+  const arr = str.split("");
+  return arr.reverse().join("");
+
+  //return str.split("").reverse().join("");
+}
 
 module.exports = reverse;
